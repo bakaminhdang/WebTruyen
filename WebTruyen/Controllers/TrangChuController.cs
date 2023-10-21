d501f176867e531e8f602c09ece83ec8dfee1395
bakaminhdang <128440576+bakaminhdang@users.noreply.github.com>
2023-10-20 20:07:15 -0700

    Add project files.

---
+++ WebTruyen/Controllers/TrangChuController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebTruyen.Controllers
+{
+    public class TrangChuController : Controller
+    {
+        // GET: TrangChu
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult NavPartial()
+        {
+            return View();
+        }
+        public ActionResult SliderPartial()
+        {
+            return PartialView();
+        }
+        public ActionResult Chitiettruyen()
+        {
+            return PartialView();
+        }
+
+    }
+}
